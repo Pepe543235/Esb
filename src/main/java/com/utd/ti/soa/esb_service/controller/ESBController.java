97b4af4a4e7e86c1e744c304cf9e3ee38e92cbe8
Pepe543235 <joxegp@email.com>
2025-03-28 12:34:51 -0600

    commit route trial

--- src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
+++ src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
@@ -74,7 +74,7 @@ public class ESBController {
 
         try {
             String response = webClient.get()
-                .uri("http://users.railway.internal:3001/api/user/all")
+                .uri("https://users-production-ec69.up.railway.app/api/user/all")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToMono(String.class)
